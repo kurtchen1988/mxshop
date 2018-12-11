100654d112232ae98c8f6cf7fbf8b60376eae978
Kurt Chen <kurtcobain1988824@hotmail.com>
2018-12-11 21:49:49 +0800

    first

--- MxShop/settings.py
+++ MxShop/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'users.apps.UsersConfig',
+    'users.apps.UsersConfig',
+    'DjangoUeditor',
+    'users',
+    'goods',
+    'trade',
+    'user_operation',
 ]
 
 MIDDLEWARE = [
--- apps/goods/models.py
+++ apps/goods/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from django.db import models
+from DjangoUeditor.models import UEditorField
 
 # Create your models here.
 class GoodsCategory(models.Model):
@@ -48,14 +49,37 @@ class Goods(models.Model):
     '''
     商品
     '''
-    category = models.ForeignKey(GoodsCategory)
-    goods_sn = models.CharField()
-    name = models.CharField()
-    click_num = models.IntegerField()
-    sold_num = models.IntegerField()
-    fav_num = models.IntegerField()
-    goods_num = models.IntegerField()
-    market_price = models.FloatField()
-    shop_price = models.FloatField()
-    goods_brief = models.TextField()
-    # goods_desc =
+    category = models.ForeignKey(GoodsCategory, verbose_name='商品类目')
+    goods_sn = models.CharField(max_length=50, default='', verbose_name='商品唯一货号')
+    name = models.CharField(max_length=300, verbose_name='商品名')
+    click_num = models.IntegerField(default=0, verbose_name='点击数')
+    sold_num = models.IntegerField(default=0, verbose_name='商品销售量')
+    fav_num = models.IntegerField(default=0, verbose_name='收藏数')
+    goods_num = models.IntegerField(default=0, verbose_name='库存书')
+    market_price = models.FloatField(default=0, verbose_name='市场价格')
+    shop_price = models.FloatField(default=0, verbose_name='本店价格')
+    goods_brief = models.TextField(max_length=500, verbose_name='商品简短描述')
+    goods_desc = UEditorField(verbose_name=u'内容', imagePath='goods/images/', filePath='goods/filter',
+                              width=1000, height=300, default='')
+    ship_free = models.BooleanField(default=True, verbose_name='是否承担运费')
+    goods_front_image = models.ImageField(upload_to='', null=True, blank=True, verbose_name='封面图片')
+    #goods_front_image_url = models.ImageField(upload_to='', default='', verbose_name='封面图')
+    is_new = models.BooleanField(default=False, verbose_name='是否新品')
+    is_hot = models.BooleanField(default=False, verbose_name='是否热销')
+
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+class GoodsImage(models.Model):
+    '''
+    商品轮播图
+    '''
+    goods = models.ForeignKey(Goods)
+    image = models.ImageField(upload_to=)
+    add_time = models.DateTimeField()
+
+    class Meta:
+        verbose_name = '商品轮播图'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.goods.name
