fa5db7ac09013bc7edc4298780f0c4d6bc20e6e9
Kurt Chen <kurtcobain1988824@hotmail.com>
2018-12-17 20:30:22 +0800

    first

--- MxShop/urls.py
+++ MxShop/urls.py
@@ -20,10 +20,13 @@ import xadmin
 from MxShop.settings import MEDIA_ROOT
 from django.views.static import serve
 
+from goods.views_base import GoodsListView
+
 urlpatterns = [
     #path('admin/', admin.site.urls),
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^media/(?P<path>.*)$', serve, {'document_root':MEDIA_ROOT}),
 
     # 商品列表
+    url(r'goods/$', GoodsListView.as_view(), name='goods-list')
 ]
