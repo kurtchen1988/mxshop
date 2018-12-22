44657e9755f469e8e89128e8f86f85136277e903
bantu <76251098@qq.com>
2018-12-22 13:23:26 +0800

    Default Changelist

--- MxShop/settings.py
+++ MxShop/settings.py
@@ -153,9 +153,9 @@ REST_FRAMEWORK = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
         #'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        #'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     )
 }
 '''
--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -6,7 +6,7 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.authentication import SessionAuthentication
 
 from .models import UserFav, UserLeavingMessage, UserAddress
-#from utils.permissions import IsOwnerOrReadOnly
+from utils.permissions import IsOwnerOrReadOnly
 from .serializers import UserFavSerializer
 # Create your views here.
 
@@ -15,10 +15,10 @@ class UserFavViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.Retr
     '''
     用户收藏功能
     '''
-    queryset = UserFav.objects.all()
-    #permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
+    #queryset = UserFav.objects.all()
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
     serializer_class = UserFavSerializer
-    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
+    #authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
     lookup_field = "goods_id"
     def get_queryset(self):
         return UserFav.objects.filter(user=self.request.user)
